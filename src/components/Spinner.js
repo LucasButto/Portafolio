62a9c29d41e13aecedcade3da3b18c434759233b
LucasButto <lbutto40@gmail.com>
2022-11-20 19:00:11 -0300

    contact form working

---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SyncLoader } from "react-spinners";
+
+const Spinner = () => {
+  return (
+    <>
+      <SyncLoader color="#3e497a" margin={2} size={25} />
+    </>
+  );
+};
+
+export default Spinner;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,69 @@
 import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
 
 import "../styles/Contact.css";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import EmailIcon from "@mui/icons-material/Email";
 import SendIcon from "@mui/icons-material/Send";
+import Spinner from "../components/Spinner";
 
 const Contact = () => {
-  const [inputName, setInputName] = useState();
-  const [inputEmail, setInputEmail] = useState();
-  const [inputMessage, setInputMessage] = useState();
+  const [inputName, setInputName] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputMessage, setInputMessage] = useState("");
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  const sendEmailHandler = () => {
-    console.log(inputName, inputEmail, inputMessage);
-    setInputName("");
-    setInputEmail("");
-    setInputMessage("");
+  const sendEmailHandler = (e) => {
+    setLoading(true);
+    e.preventDefault();
+    let data = {
+      user_name: inputName,
+      user_email: inputEmail,
+      message: inputMessage,
+    };
+
+    let validate = validateDataForm(data);
+    setErrors(validate);
+
+    if (Object.entries(validate).length === 0) {
+      emailjs
+        .sendForm(
+          "service_dp3aw3l",
+          "template_7egq351",
+          e.target,
+          "_dTu7J3vq-AlfVvXa"
+        )
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
+      setInputName("");
+      setInputEmail("");
+      setInputMessage("");
+    }
+    setLoading(false);
+  };
+
+  const validateDataForm = (form) => {
+    let _errors = {};
+    if (form.user_name === "") {
+      _errors.name = "Required field.";
+    }
+    if (form.user_email === "") {
+      _errors.email = "Required field.";
+    } else if (emailValidation(form.user_email) === false) {
+      _errors.email = "Enter a valid email address.";
+    }
+    if (form.message === "") {
+      _errors.message = "Required field.";
+    }
+
+    return _errors;
+  };
+
+  const emailValidation = (email) => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    return emailRegex.test(email);
   };
 
   return (
@@ -24,52 +72,51 @@ const Contact = () => {
         <h1 className="section-header">Contact</h1>
 
         <div className="contact-wrapper">
-          <form id="contact-form" className="form-horizontal">
-            <div className="form-inputs">
-              <input
-                type="text"
-                className="form-input"
-                id="name"
-                placeholder="NAME"
-                name="name"
-                value={inputName}
-                onChange={(e) => setInputName(e.target.value)}
-                required
-              />
+          <div className="contact-form">
+            <form className="form-horizontal" onSubmit={sendEmailHandler}>
+              <div className="form-inputs">
+                <input
+                  type="text"
+                  className="form-input"
+                  placeholder="NAME"
+                  name="user_name"
+                  value={inputName}
+                  onChange={(e) => setInputName(e.target.value)}
+                />
+                {errors.name && <p className="error">{errors.name}</p>}
 
-              <input
-                type="email"
-                className="form-input"
-                id="email"
-                placeholder="EMAIL"
-                name="email"
-                value={inputEmail}
-                onChange={(e) => setInputEmail(e.target.value)}
-                required
-              />
+                <input
+                  type="email"
+                  className="form-input"
+                  placeholder="EMAIL"
+                  name="user_email"
+                  value={inputEmail}
+                  onChange={(e) => setInputEmail(e.target.value)}
+                />
+                {errors.email && <p className="error">{errors.email}</p>}
 
-              <textarea
-                className="form-text"
-                placeholder="MESSAGE"
-                value={inputMessage}
-                onChange={(e) => setInputMessage(e.target.value)}
-                required
-              ></textarea>
-            </div>
-
-            <button
-              className="send-button"
-              id="submit"
-              type="button"
-              value="SEND"
-              onClick={sendEmailHandler}
-            >
-              <div className="alt-send-button">
-                <SendIcon />
-                <span className="send-text">SEND</span>
+                <textarea
+                  className="form-text"
+                  placeholder="MESSAGE"
+                  name="message"
+                  value={inputMessage}
+                  onChange={(e) => setInputMessage(e.target.value)}
+                ></textarea>
+                {errors.message && <p className="error">{errors.message}</p>}
               </div>
-            </button>
-          </form>
+
+              <button className="send-button">
+                <div className="alt-send-button">
+                  <SendIcon />
+                </div>
+              </button>
+            </form>
+            {loading && (
+              <div className="spinner">
+                <Spinner />
+              </div>
+            )}
+          </div>
 
           <div className="direct-contact-container">
             <ul className="contact-list">
