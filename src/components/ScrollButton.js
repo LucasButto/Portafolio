e3695b7825252b4ac5747d9e2ba9e57079679fa8
LucasButto <lbutto40@gmail.com>
2023-01-27 23:45:12 -0300

    fade up animation to scroll button

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -11,7 +11,7 @@ const ScrollButton = () => {
       };
   return (
     <>
-      <button onClick={scrollToTop} className="scroll-button"> 
+      <button onClick={scrollToTop} className="scroll-button" data-aos="fade-up" data-aos-duration="500"> 
         <KeyboardArrowUpIcon className="arrow1"/>
         <KeyboardArrowUpIcon className="arrow2"/>
       </button>
